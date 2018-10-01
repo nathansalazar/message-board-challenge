761fbeedc1bbf3a3c3374b4a61a0a77063cf2e58
Nathan Salazar <nathan.salazar20@gmail.com>
2018-10-01 11:15:56 -0500

    give random background colors to each person

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -4,6 +4,7 @@ app.controller('MessagingController',['$http',function($http){
     let vm = this;
     vm.message = 'Angularjs is working';
     vm.messages=[];
+    vm.names=[];
 
     vm.postMessage = function(message){
         console.log('Name:',message.name);
@@ -18,8 +19,39 @@ app.controller('MessagingController',['$http',function($http){
     } 
 
     vm.getMessages=function(){
+        let names=[];
+        let nameColor = [];
         $http.get('/messageBoard').then(function(response){
             vm.messages = response.data;
+
+            //The code below is my attempt to give each person a random
+            //background color that will show up for all of their messages
+
+            
+            //get list of all different names
+            for(message of vm.messages){
+                if(!names.includes(message.name)){
+                    names.push(message.name);
+                    nameColor.push({name: message.name});
+                }
+            }
+            //assign each name a color
+            for(i=0;i<nameColor.length;i++){
+                nameColor[i].color = 'rgb('+(10*i)%255+','+(25*i)%255+','+(50*i)%255+')';
+            }
+            //pass that color into the vm.messages array
+            for(message of vm.messages){
+                message.color = 'red';
+                for(object of nameColor){
+                    console.log('In second for loop');
+                    if(message.name == object.name){
+                        console.log(message.name,'is the same as',object.name);
+                        message.color = object.color;
+                    }
+                }
+            }
+            console.log('The different names are:', names);
+            console.log('vm.messages is',vm.messages);
         })
     }
     vm.getMessages();
