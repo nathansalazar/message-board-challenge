e2712797032cdb12d79b3f297f0378708a6b0944
Nathan Salazar <nathan.salazar20@gmail.com>
2018-10-01 10:33:46 -0500

    set up GET and POST routes

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -3,4 +3,24 @@ const app = angular.module('MessagingApp',[]);
 app.controller('MessagingController',['$http',function($http){
     let vm = this;
     vm.message = 'Angularjs is working';
+    vm.messages=[];
+
+    vm.postMessage = function(message){
+        console.log('Name:',message.name);
+        console.log('Message:',message.message);
+        $http.post('/messageBoard',message).then(function(response){
+            vm.getMessages();
+        }).catch(function(error){
+            console.log('Error in POST:',error);
+        })
+
+        vm.newMessage={};
+    } 
+
+    vm.getMessages=function(){
+        $http.get('/messageBoard').then(function(response){
+            vm.messages = response.data;
+        })
+    }
+    vm.getMessages();
 }])
--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-const pg = require('pg');
+const pool = require('./modules/pool.js');
 
 app.use(express.static('server/public'));
 app.use(bodyParser.json());
@@ -10,5 +10,25 @@ const port = 5000;
 
 app.listen(port,()=>{
     console.log('Server running on port',port);
+})
+
+//routes
+
+app.get('/messageBoard',(req,res)=>{
+    pool.query(`SELECT * FROM "messages";`).then((results)=>{
+        res.send(results.rows);
+    }).catch((error)=>{
+        console.log('Error in GET:',error);
+    })
+})
+
+app.post('/messageBoard',(req,res)=>{
+    pool.query(`INSERT INTO "messages"
+    ("name", "message") VALUES 
+    ($1,$2);`,[req.body.name,req.body.message]).then((results)=>{
+        res.sendStatus(201);
+    }).catch((error)=>{
+        console.log('Error in POST:',error);
+    });
 })
 
